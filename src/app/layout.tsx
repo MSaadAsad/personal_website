3df8ae80a9cdcf1688678a5ce212ad95141919ff
Your Name <muhammad163@uni.minerva.edu>
2026-03-14 14:58:28 -0700

    api change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Script
           src="https://www.bayanista.com/sdk/v1/bayanista.min.js"
           data-project="7f87e047-3784-4f41-82a6-24bc31879e44"
-          data-api-key="bay_kXq-Q8Dcp9JI_y-e2UASPKXWn9pwovKj"
+          data-api-key="bayanista_oefDYUWaK3y7L0wDOB_6RVUjUvcG56p6EBv23y735MQ"
           data-auto-init
           strategy="afterInteractive"
         />
