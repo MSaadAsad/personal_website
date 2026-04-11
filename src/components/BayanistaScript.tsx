866eea7a8a6f25f1773ca4e2f3d9d00bbc30d75e
Your Name <muhammad163@uni.minerva.edu>
2026-04-10 19:33:54 -0700

    ts

--- src/components/BayanistaScript.tsx
+++ src/components/BayanistaScript.tsx
@@ -3,11 +3,14 @@
 import Script from 'next/script';
 
 export function BayanistaScript() {
+  const projectId = process.env.NEXT_PUBLIC_BAYANISTA_PROJECT_ID;
+  const apiKey = process.env.NEXT_PUBLIC_BAYANISTA_API_KEY;
+
   return (
     <Script
       src="https://www.bayanista.com/sdk/v1/bayanista.min.js"
-      data-project="7f87e047-3784-4f41-82a6-24bc31879e44"
-      data-api-key="bayanista_H0AT73ICH_dtd49MacAAXurJbOaSu5MlZ3wrYVrtQvI"
+      data-project={projectId}
+      data-api-key={apiKey}
       data-auto-init
       strategy="afterInteractive"
       onLoad={() => window.dispatchEvent(new Event('bayanista:ready'))}
